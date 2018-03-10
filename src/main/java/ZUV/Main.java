fe2b3e792647fc4bb5aa41b40e7804644f50dacf
Jochen Stärk <jstaerk@usegroup.de Jochen Staerk>
2018-03-10 21:02:09 +0100

    display xml validation output as xml

--- src/main/java/ZUV/Main.java
+++ src/main/java/ZUV/Main.java
@@ -1,11 +1,19 @@
 package ZUV;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.EnumSet;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,6 +22,26 @@ import javax.xml.transform.stream.StreamSource;
 import org.mustangproject.ZUGFeRD.ZUGFeRDImporter;
 import org.oclc.purl.dsdl.svrl.FailedAssert;
 import org.oclc.purl.dsdl.svrl.SchematronOutputType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.impl.StaticLoggerBinder;
+import org.verapdf.core.VeraPDFException;
+import org.verapdf.features.FeatureExtractorConfig;
+import org.verapdf.features.FeatureFactory;
+import org.verapdf.features.FeatureObjectType;
+import org.verapdf.metadata.fixer.FixerFactory;
+import org.verapdf.metadata.fixer.MetadataFixerConfig;
+import org.verapdf.pdfa.VeraGreenfieldFoundryProvider;
+import org.verapdf.pdfa.validation.validators.ValidatorConfig;
+import org.verapdf.pdfa.validation.validators.ValidatorFactory;
+import org.verapdf.processor.BatchProcessor;
+import org.verapdf.processor.FormatOption;
+import org.verapdf.processor.ProcessorConfig;
+import org.verapdf.processor.ProcessorFactory;
+import org.verapdf.processor.TaskType;
+import org.verapdf.processor.plugins.Attribute;
+import org.verapdf.processor.plugins.PluginConfig;
+import org.verapdf.processor.plugins.PluginsCollectionConfig;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -25,14 +53,14 @@ import com.sanityinc.jargs.CmdLineParser.Option;
 public class Main {
 
 	static final ClassLoader cl = Main.class.getClassLoader();
-	private static final Logger LOGGER = Logger.getLogger(Main.class.getCanonicalName());
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class.getCanonicalName());
 
 	public static void main(String[] args) {
 
 		/***
 		 * prerequisite is a mvn generate-resources
 		 */
-
+		
 		CmdLineParser parser = new CmdLineParser();
 		Option<String> filenameOption = parser.addStringOption('f', "filename");
 		try {
@@ -49,13 +77,58 @@ public class Main {
 			System.exit(-1);
 		}
 
-		System.out.println("Validating...");
+
+		System.err.println("<validation><pdf>");
+		// Validate PDF
+
+		VeraGreenfieldFoundryProvider.initialise();
+		// Default validator config
+		ValidatorConfig validatorConfig = ValidatorFactory.defaultConfig();
+		// Default features config
+		FeatureExtractorConfig featureConfig = FeatureFactory.defaultConfig();
+		// Default plugins config
+		PluginsCollectionConfig pluginsConfig = PluginsCollectionConfig.defaultConfig();
+		// Default fixer config
+		MetadataFixerConfig fixerConfig = FixerFactory.defaultConfig();
+		// Tasks configuring
+		EnumSet tasks = EnumSet.noneOf(TaskType.class);
+		tasks.add(TaskType.VALIDATE);
+//		tasks.add(TaskType.EXTRACT_FEATURES);
+		//tasks.add(TaskType.FIX_METADATA);
+		// Creating processor config
+		ProcessorConfig processorConfig = ProcessorFactory.fromValues(validatorConfig, featureConfig, pluginsConfig, fixerConfig, tasks);
+		// Creating processor and output stream.
+		ByteArrayOutputStream reportStream = new ByteArrayOutputStream();
+		String pdfReport="";
+		try (BatchProcessor processor = ProcessorFactory.fileBatchProcessor(processorConfig)) {
+			// Generating list of files for processing
+			List<File> files = new ArrayList<>();
+			files.add(new File(fileName));
+			// starting the processor
+			processor.process(files, ProcessorFactory.getHandler(FormatOption.MRR, true, reportStream,
+							100, processorConfig.getValidatorConfig().isRecordPasses()));
+
+			pdfReport=reportStream.toString("utf-8").replaceAll("<\\?xml version=\"1\\.0\" encoding=\"utf-8\"\\?>", "");
+		} catch (VeraPDFException e) {
+			System.err.println("<exception message='"+e.getMessage()+"'>"+e.getStackTrace()+"</exception>");
+			
+			LOGGER.error(e.getMessage());
+		} catch (IOException excep) {
+			System.err.println("<exception message='"+excep.getMessage()+"'>"+excep.getStackTrace()+"</exception>");
+			LOGGER.error(excep.getMessage());
+		}
+		
+		
+		// Validate ZUGFeRD
+		System.err.println(pdfReport+"</pdf><xml>");
+		
 		ZUGFeRDImporter zi = new ZUGFeRDImporter();
 		zi.extract(fileName);
-		System.out.println("Reading ZUGFeRD");
 		if (zi.canParse()) {
-			System.out.println(validateZUGFeRD(zi.getMeta()));
+			System.err.println(validateZUGFeRD(zi.getMeta()));
 		}
+		System.err.println("</xml>");
+		System.err.println("</validation>");
 
 	}
 
@@ -116,25 +189,29 @@ public class Main {
 					throw new IllegalArgumentException("Invalid Schematron!");
 				}
 
+				
 				SchematronOutputType sout = aResSCH
 						.applySchematronValidationToSVRL(new StreamSource(new StringReader(xmlString)));
 				List<Object> failedAsserts = sout.getActivePatternAndFiredRuleAndFailedAssert();
 				for (Object object : failedAsserts) {
 					if (object instanceof FailedAssert) {
+
 						FailedAssert failedAssert = (FailedAssert) object;
-						schematronValidationString += failedAssert.getText() + "\n";
-						schematronValidationString += failedAssert.getTest() + "\n";
+
+						schematronValidationString += "<error><criterion>"+failedAssert.getTest() +"</criterion><result>"+failedAssert.getText() + "</result>\n";
 					}
+					
 				}
 				for (String currentString : sout.getText()) {
 
-					schematronValidationString += currentString;
+					schematronValidationString += "<output>"+currentString+"</output>";
 				}
 
 			} catch (
 
 			Exception ex) {
-				ex.printStackTrace();
+				schematronValidationString += "<exception message='"+ex.getMessage()+"'>"+ex.getStackTrace()+"</exception>";
+				LOGGER.error(ex.getMessage());
 			}
 			rightNow = Calendar.getInstance();
 			long endTime = rightNow.getTimeInMillis();
